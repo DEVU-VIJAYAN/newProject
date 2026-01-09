e687d59192ec7e2bf308634d1d2c6dc5ea7d4329
DEVU-VIJAYAN <134757571+DEVU-VIJAYAN@users.noreply.github.com>
2026-01-09 11:12:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-<div>
-
-  <h1>
-  NEW PROJECT
-  </h1>
-
-
-
-</div>
+    <div className="container">
+      <h1 className="title">DEVU VIJAYAN</h1>
+      <p className="subtitle">DevOps • CI/CD • Docker • Jenkins</p>
+    </div>
   )
 }
 
 export default App
+
